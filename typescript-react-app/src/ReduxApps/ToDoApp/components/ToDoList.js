056fe19f6c07d4c991f812759470e5c3189faa29
MrCoul3 <mr.coul@inbox.ru>
2021-09-11 08:41:59 +0300

    iteration 12/13

--- typescript-react-app/src/ReduxApps/ToDoApp/components/ToDoList.js
+++ typescript-react-app/src/ReduxApps/ToDoApp/components/ToDoList.js
@@ -1,13 +1,19 @@
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import React from "react";
 
 const selector = state => state;
 
 export default function ToDoList() {
+    const dispatch = useDispatch();
+
+    const handleSelectNote = () => {
+        dispatch({type: 'selectNote', selected: true})
+    }
 
     const select = useSelector(selector);
+
     console.log(select)
-    const render = select.map(obj => <div>{obj.text}</div>)
+    const render = select.map(obj => <div onClick={handleSelectNote}>{obj.text}</div>)
 
 
     return (
--- typescript-react-app/src/ReduxApps/ToDoApp/reducer.js
+++ typescript-react-app/src/ReduxApps/ToDoApp/reducer.js
@@ -1,7 +1,11 @@
 export default  function counterReducer(state = [], action) {
 
     if (action.type === 'add') {
-        return [...state, {text: action.payload}]
+        return [...state, {text: action.payload, select: false}]
+    }
+
+    if (action.type === 'selectNote') {
+        return [...state]
     }
 
     return state;
